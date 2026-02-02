d8dcbeeee10475795539741b84ff228f6ce20fbf
Aditya <workforadityaa18@gmail.com>
2026-02-03 01:13:58 +0530

    Added useEffect import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Hero from './components/Hero';
 import Wishes from './components/Wishes';
 import CelestialReasons from './components/CelestialReasons';
